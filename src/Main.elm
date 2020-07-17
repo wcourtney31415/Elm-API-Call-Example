0bc245f93c3e622a92fd1fb84d6b7821fa8ac376
unknown <wcourtney31415@gmail.com>
2020-07-17 03:46:00 -0400

    Fixed the API call after the API's url had been moved to a new domain. Heavily modified the code and layout.

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,24 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Browser
-import Element exposing (..)
+import Element as E
 import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 import Element.Input as Input
 import Html exposing (Html)
 import Http
-import Json.Decode exposing (Decoder, field, int, map2, string)
+import Json.Decode as JsonDecoder
+    exposing
+        ( Decoder
+        , field
+        , map2
+        , string
+        )
+import Style as S
 
 
+main : Program () Model Msg
 main =
     Browser.element
         { init = init
@@ -25,26 +35,15 @@ type ApiCallState
 
 
 type alias Model =
-    { apiCallState : ApiCallState, characters : List Character }
-
-
-exampleCharacter =
-    { name = "Kyle Katarn"
-    , height = "40"
+    { apiCallState : ApiCallState
+    , characters : List Character
     }
 
 
-initialModel =
-    { apiCallState = Loading, characters = [ exampleCharacter ] }
-
-
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( initialModel, getCharacter )
-
-
-apiUrl =
-    "https://swapi.co/api/people/20/"
+type alias Character =
+    { name : String
+    , height : String
+    }
 
 
 type Msg
@@ -52,60 +51,124 @@ type Msg
     | GotCharacter (Result Http.Error Character)
 
 
+exampleCharacter : { name : String, height : String }
+exampleCharacter =
+    { name = "Kyle Katarn"
+    , height = "40"
+    }
+
+
+initialModel : { apiCallState : ApiCallState, characters : List Character }
+initialModel =
+    { apiCallState = Loading
+    , characters = [ exampleCharacter ]
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel, getCharacter )
+
+
+apiUrl : String
+apiUrl =
+    "https://swapi.dev/api/people/20/"
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         RequestCharacter ->
-            ( { model | apiCallState = Loading }, getCharacter )
+            ( { model | apiCallState = Loading }
+            , getCharacter
+            )
 
         GotCharacter result ->
             case result of
                 Ok character ->
-                    ( { model | apiCallState = Success, characters = character :: model.characters }, Cmd.none )
+                    ( { model
+                        | apiCallState = Success
+                        , characters = character :: model.characters
+                      }
+                    , Cmd.none
+                    )
 
                 Err _ ->
-                    ( { model | apiCallState = Failure }, Cmd.none )
+                    ( { model | apiCallState = Failure }
+                    , Cmd.none
+                    )
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
+subscriptions _ =
     Sub.none
 
 
 view : Model -> Html Msg
 view model =
     let
-        myStr =
+        tup =
             case model.apiCallState of
                 Success ->
-                    "Success"
+                    ( "Success", S.green )
 
                 Failure ->
-                    "Failure"
+                    ( "Failure", S.red )
 
                 Loading ->
-                    "Contacting API"
-    in
-    Element.layout []
-        (Element.column [ centerX, centerY, spacing 20 ]
-            [ Input.button
-                [ centerX
-                , Background.color (rgb255 219 219 219)
-                , padding 10
+                    ( "Contacting API", S.blue )
+
+        col =
+            Tuple.second tup
+
+        txt =
+            Tuple.first tup
+
+        pageState =
+            E.el
+                [ E.centerX
+                , Background.color col
+                , E.padding 10
+                , Font.bold
+                , Border.rounded 5
+                , S.shadow
                 ]
-                { onPress = Just RequestCharacter
-                , label = text "Get Character"
-                }
-            , el [ centerX ] (text myStr)
-            , Element.column [ spacing 10 ]
-                (List.map characterToString model.characters)
+            <|
+                E.text txt
+    in
+    E.layout [ Background.color S.grey ]
+        (E.column
+            [ E.centerX
+            , E.centerY
+            , E.spacing 20
+            ]
+            [ getCharButton
+            , pageState
+            , characterList model
             ]
         )
 
 
-characterToString : Character -> Element msg
-characterToString character =
-    text ("Name: " ++ character.name ++ " Height: " ++ character.height)
+characterList : Model -> E.Element Msg
+characterList model =
+    E.column
+        [ E.spacing 10
+        ]
+        (List.map characterToText model.characters)
+
+
+getCharButton : E.Element Msg
+getCharButton =
+    Input.button
+        [ E.centerX
+        , Background.color S.btnGrey
+        , E.padding 10
+        , Border.rounded 5
+        , S.shadow
+        ]
+        { onPress = Just RequestCharacter
+        , label = E.el [ Font.size 20, Font.bold ] <| E.text "Get Character"
+        }
 
 
 getCharacter : Cmd Msg
@@ -116,14 +179,34 @@ getCharacter =
         }
 
 
-type alias Character =
-    { name : String
-    , height : String
-    }
-
-
 characterDecoder : Decoder Character
 characterDecoder =
-    map2 Character
+    JsonDecoder.map2 Character
         (field "name" string)
         (field "height" string)
+
+
+ffield ( fieldName, val ) =
+    E.row
+        [ E.spacing 5 ]
+        [ E.el [ Font.bold ] <|
+            E.text <|
+                fieldName
+                    ++ ":"
+        , E.text val
+        ]
+
+
+characterToText : Character -> E.Element msg
+characterToText character =
+    E.el
+        [ Background.color S.purple
+        , Border.rounded 10
+        , E.padding 10
+        , S.shadow
+        ]
+    <|
+        E.column [ E.spacing 10 ]
+            [ ffield ( "Name", character.name )
+            , ffield ( "Height", character.height )
+            ]
